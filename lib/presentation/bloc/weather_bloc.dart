98010780e88478979dfc3c308ce161e2e35b5566
Lucas Marianno <lucas.marianno94@gmail.com>
2024-08-12 22:37:13 -0300

    bloc test

--- lib/presentation/bloc/weather_bloc.dart
+++ lib/presentation/bloc/weather_bloc.dart
@@ -1,3 +1,4 @@
+import 'package:rxdart/rxdart.dart';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_tdd_clean_architecture/domain/entities/weather.dart';
@@ -10,11 +11,28 @@ class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
   final GetCurrentWeatherUseCase _getCurrentWeatherUseCase;
 
   WeatherBloc(this._getCurrentWeatherUseCase) : super(WeatherEmpty()) {
-    on<CityChanged>(_onCityChanged);
+    on<CityChanged>(
+      _onCityChanged,
+      transformer: debounce(const Duration(milliseconds: 500)),
+    );
   }
+
   _onCityChanged(CityChanged event, Emitter<WeatherState> emit) async {
     emit(WeatherLoading());
 
     final result = await _getCurrentWeatherUseCase.execute(event.cityName);
+
+    result.fold(
+      (failure) {
+        emit(WeatherLoadFailure(failure.message));
+      },
+      (data) {
+        emit(WeatherLoaded(data));
+      },
+    );
+  }
+
+  EventTransformer<T> debounce<T>(Duration duration) {
+    return (events, mapper) => events.debounceTime(duration).flatMap(mapper);
   }
 }
--- test/presentation/bloc/weather_bloc_test.dart
+++ test/presentation/bloc/weather_bloc_test.dart
@@ -1,7 +1,78 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter_tdd_clean_architecture/core/error/failure.dart';
+import 'package:flutter_tdd_clean_architecture/domain/entities/weather.dart';
 import 'package:flutter_tdd_clean_architecture/domain/usecases/get_current_weather.dart';
 import 'package:flutter_tdd_clean_architecture/presentation/bloc/weather_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../helper/test_helper.mocks.dart';
 
 void main() {
+  late MockWeatherRepository mockWeatherRepository;
   late GetCurrentWeatherUseCase getCurrentWeatherUseCase;
   late WeatherBloc weatherBloc;
+
+  setUp(() {
+    mockWeatherRepository = MockWeatherRepository();
+    getCurrentWeatherUseCase = GetCurrentWeatherUseCase(mockWeatherRepository);
+    weatherBloc = WeatherBloc(getCurrentWeatherUseCase);
+  });
+
+  const testWeatherEntity = WeatherEntity(
+    cityName: 'São Paulo',
+    main: 'Clear',
+    description: 'clear sky',
+    iconCode: '01n',
+    temperature: 295.42,
+    pressure: 1014,
+    humidity: 57,
+  );
+
+  const testCityName = 'Sao Paulo';
+
+  group('WeatherBloc test', () {
+    test('initial state should be empty', () {
+      expect(weatherBloc.state, WeatherEmpty());
+    });
+
+    blocTest<WeatherBloc, WeatherState>(
+      'should emit [WeatherLoading, WeatherLoaded] when data get is sucessful',
+      build: () {
+        when(
+          getCurrentWeatherUseCase.execute(testCityName),
+        ).thenAnswer(
+          (_) async => const Right(testWeatherEntity),
+        );
+
+        return weatherBloc;
+      },
+      act: (bloc) => bloc.add(const CityChanged(testCityName)),
+      wait: const Duration(milliseconds: 500),
+      expect: () => [
+        WeatherLoading(),
+        const WeatherLoaded(testWeatherEntity),
+      ],
+    );
+
+    blocTest<WeatherBloc, WeatherState>(
+      'should emit [WeatherLoading, WeatherLoadFailure] when data get fails',
+      build: () {
+        when(
+          getCurrentWeatherUseCase.execute(testCityName),
+        ).thenAnswer(
+          (_) async => const Left(ServerFailure('Server failure')),
+        );
+
+        return weatherBloc;
+      },
+      act: (bloc) => bloc.add(const CityChanged(testCityName)),
+      wait: const Duration(milliseconds: 500),
+      expect: () => [
+        WeatherLoading(),
+        const WeatherLoadFailure('Server failure'),
+      ],
+    );
+  });
 }
